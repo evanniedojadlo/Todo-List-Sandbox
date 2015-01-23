1041bdbe87ea71f3de65629e52b8673e77adc5d8
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-01-22 23:59:39 -0600

    Added default options, will need to cleanup and make test pass.

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,6 +1,28 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
+	def update_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list."
+
+
+	within "#todo_list#{todo_list.id}" do
+		click_link "Edit"
+	end
+
+			within "#todo_list_#{todo_list.id}" do
+			click_link "Edit"
+		end
+
+		fill_in "Title", with: "New title"
+		fill_in "Description", with: "New description"
+		click_button "Update Todo list"
+
+	end
+
+
+
+
 	it "updates a todo list successfully with correct information" do
 		visit "/todo_lists"
 		todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
@@ -14,7 +36,7 @@ describe "Editing todo lists" do
 		fill_in "Description", with: "New description"
 		click_button "Update Todo list"
 
-		todo_list.reload
+		todo_list.reload #normally used within Tests only
 
 		expect(page).to have_content("Todo list was successfully updated")
 		expect(todo_list.title).to eq("New title")
