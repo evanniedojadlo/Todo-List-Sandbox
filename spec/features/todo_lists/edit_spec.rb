51356ab88d447a924542f9a8ee8f1ab9984b9c8b
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-01-23 01:34:35 -0600

    Failing edit post test

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -5,36 +5,28 @@ describe "Editing todo lists" do
 		options[:title] ||= "My todo list"
 		options[:description] ||= "This is my todo list."
 
+		todo_list = options[:todo_list]
 
-	within "#todo_list#{todo_list.id}" do
+		visit "/todo_lists"
+		within "#todo_list#{todo_list.id}" do
 		click_link "Edit"
 	end
 
-			within "#todo_list_#{todo_list.id}" do
-			click_link "Edit"
-		end
-
-		fill_in "Title", with: "New title"
-		fill_in "Description", with: "New description"
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Update Todo list"
-
 	end
 
 
 
 
 	it "updates a todo list successfully with correct information" do
-		visit "/todo_lists"
+
 		todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
 
-		visit "/todo_lists"
-		within "#todo_list_#{todo_list.id}" do
-			click_link "Edit"
-		end
-
-		fill_in "Title", with: "New title"
-		fill_in "Description", with: "New description"
-		click_button "Update Todo list"
+		update_todo_list todo_list: todo_list, 
+							title: "New title", 
+							description: "New description"
 
 		todo_list.reload #normally used within Tests only
 
