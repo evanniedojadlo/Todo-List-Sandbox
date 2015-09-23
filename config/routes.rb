373205583f5fe7b9921d4d953f4e2a0c59e0c3fb
evanniedojadlo <evanniedojadlo@gmail.com>
2015-09-23 14:28:29 -0500

    Added a nested resource within routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ODOT::Application.routes.draw do
-  get "todo_items/index"
-  resources :todo_lists
-
+  resources :todo_lists do
+    resources :todo_items
+  end
   root 'todo_lists#index'
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
